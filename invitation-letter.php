1adccaedd017619406a097534e63b948032b3c07
simayaksoz <simay.aksoz@st.uskudar.edu.tr>
2024-03-25 11:10:41 +0300

    düzeltmeler yapıldı

--- invitation-letter.php
+++ invitation-letter.php
@@ -2,7 +2,7 @@
     <div class="xs-service-content">
         <p>To Whom It May Concern,</p>
         <p><b>2nd International Pediatric Updates Symposium</b> jointly organized by <b>The Society of Pediatric Allergy Immunology and Asthma</b> and the <b>Macedonia Pediatrics Association</b>, will take place from 12-13 June, 2024, at Rixos Premium Hotel in Croatia, Dubrovnik. </p>
-        <p>A total of 180 healthcare professionals, 60 from Turkey, 60 from Macedonia,30 from Croatia, 20 from Serbia, 10 from Montenegro, are expected to attend our meeting.</p>
+        <p>A total of 180 healthcare professionals, 61 from Turkey, 60 from Macedonia, 29 from Croatia, 20 from Serbia, 10 from Montenegro, are expected to attend our meeting.</p>
         <p>We are planning to organize a productive conference that brings together colleagues from both countries for scientific exchange, sharing of professional experiences and transferring expertise in the field of pediatric development in both countries.</p>
         <p>Sincerely,</p>
         <p><b>Prof. Dr. Koray Harmancı<br>
